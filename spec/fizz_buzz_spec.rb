e87450518f4b345b6948795a2020acb03b5a920c
george <georgeaustinbarrett@gmail.com>
2023-03-30 12:05:23 +0100

    test cases added for fizz_buzz.rb

--- spec/fizz_buzz_spec.rb
+++ spec/fizz_buzz_spec.rb
@@ -5,3 +5,15 @@ describe 'fizz_buzz' do
     expect(fizz_buzz(3)).to eq 'fizz'
   end
 end
+
+describe 'fizz_buzz' do
+  it 'returns "buzz" when passed 5' do
+    expect(fizz_buzz(5)).to eq 'buzz'
+  end
+end
+
+describe 'fizz_buzz' do
+  it 'returns "fizzbuzz" when passed 15' do
+    expect(fizz_buzz(15)).to eq 'fizzbuzz'
+  end
+end
