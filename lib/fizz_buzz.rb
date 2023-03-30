016786b5495b5600da8ec2e718674a6cf3972a8f
sharmine-s <sharmine.ss@gmail.com>
2023-03-30 12:22:29 +0100

    fizzbuzz program completed

--- lib/fizz_buzz.rb
+++ lib/fizz_buzz.rb
@@ -1,3 +1,10 @@
 def fizz_buzz(number)
-  'fizz'
+  case 
+  when number % 3 == 0 && number % 5 == 0 
+    return "fizzbuzz"
+  when number % 3 == 0 
+    return "fizz"
+  when number % 5 == 0 
+    return "buzz"
+  end
 end
