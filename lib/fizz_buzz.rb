90cbc602b30a4a91215f7d35bce63bff488e6571
sharmine-s <sharmine.ss@gmail.com>
2023-03-30 12:52:51 +0100

    added statements for when number is smaller than 1 or not an integer

--- lib/fizz_buzz.rb
+++ lib/fizz_buzz.rb
@@ -1,10 +1,15 @@
 def fizz_buzz(number)
   case 
+  when !(number.is_a? Integer) || number == 0 || number < 1
+    return "please input a positive integer"
   when number % 3 == 0 && number % 5 == 0 
     return "fizzbuzz"
   when number % 3 == 0 
     return "fizz"
   when number % 5 == 0 
     return "buzz"
+  else
+    return number
   end
 end
+
